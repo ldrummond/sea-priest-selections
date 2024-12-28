f84e7a0f7b2722859e0e4972deabd000e2ce06f4
Lucas Drummond <lucas@consumeandcreate.co>
2024-12-27 19:03:20 -0600

    Progress

--- src/main.js
+++ src/main.js
@@ -1,23 +1,61 @@
 import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
 
-// document.querySelector('#app').innerHTML = `
-//   <div>
-//     <a href="https://vite.dev" target="_blank">
-//       <img src="${viteLogo}" class="logo" alt="Vite logo" />
-//     </a>
-//     <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-//       <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-//     </a>
-//     <h1>Hello Vite!</h1>
-//     <div class="card">
-//       <button id="counter" type="button"></button>
-//     </div>
-//     <p class="read-the-docs">
-//       Click on the Vite logo to learn more
-//     </p>
-//   </div>
-// `
-// setupCounter(document.querySelector('#counter'))
+"use strict";
+
+document.addEventListener("DOMContentLoaded", () => {
+
+});
+
+
+// https://gist.github.com/getify/150ea5a3b30b8822dee7798883d120b9
+function computeViewportDimensions() {
+   if (document.documentElement && document.documentElement.style && document.documentElement.style.setProperty) {
+      document.documentElement.style.setProperty(
+         "--vw-unit",
+         (document.documentElement.clientWidth / 100).toFixed(1) + "px"
+      );
+      document.documentElement.style.setProperty(
+         "--vh-unit",
+         (document.documentElement.clientHeight / 100).toFixed(1) + "px"
+      );
+   }
+}
+
+(function listenForViewportChanges(){
+   // keep the CSS vw-unit/vh-unit CSS variables updated as the viewport changes size (or orientation!)
+   window.addEventListener("resize",computeViewportDimensions,false);
+
+   // work-arounds for browsers that don't fire "resize" when the orientation changes
+   // ref: https://developer.mozilla.org/en-US/docs/Web/API/ScreenOrientation/onchange
+   if (typeof window.screen != "undefined" && typeof window.screen.orientation != "undefined") {
+      window.screen.orientation.addEventListener("change",computeViewportDimensions,false);
+   }
+   // ref: https://www.reddit.com/r/javascript/comments/lttxdy/js_workaround_for_fixing_how_css_vwvh_units_arent/gp61ghe/
+   // ref: https://developer.mozilla.org/en-US/docs/Web/API/MediaQueryList/matches
+   else if (typeof window.matchMedia != "undefined") {
+      var query = window.matchMedia("(orientation: landscape)");
+
+      // handle variances in the event handling in various older browsers
+      if (typeof query.addEventListener != "undefined") {
+         query.addEventListener("change",computeViewportDimensions,false);
+      }
+      else if (typeof query.addListener != "undefined") {
+         query.addListener(computeViewportDimensions);
+      }
+      else {
+         query.onchange = computeViewportDimensions;
+      }
+   }
+    
+   // make sure nothing during HTML parsing invalidated the early
+   // computation (from the <script> embed)
+   //
+   // has the DOM already loaded?
+   if (document.readyState != "loading") {
+      computeViewportDimensions();
+   }
+   // otherwise, assume we can listen for the future DOM-ready event
+   else {
+      document.addEventListener("DOMContentLoaded",computeViewportDimensions,false);
+   }
+})();
